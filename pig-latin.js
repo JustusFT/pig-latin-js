1ccbb7b959d1b9faf85f9d6bec3043f27b633b78
JustusFT <JustusFT@gmail.com>
2017-04-07 16:12:07 +0800

    finished exercise

--- pig-latin.js
+++ pig-latin.js
@@ -2,11 +2,17 @@ function getPigLatin() {
 
   //get the word input by the user
   var str = document.getElementById("word").value;
-  
+
 
   //your code goes here
+  //do not modify words that start with a vowel
+  if(!/[AEIOU]/i.test(str[0])) {
+    firstHalf = str.match(/[^aeiou]+/i);
+    secondHalf = str.replace(/[^aeiou]+/i, "");
+    str = secondHalf + firstHalf + "ay";
+  }
 
-  //change the HTML content of a <p> element with id="pig-latin-word": 
+  //change the HTML content of a <p> element with id="pig-latin-word":
   document.getElementById("pig-latin-word").innerHTML = str;
 
 } //end of function getPigLatin()
